bef7ce5025845193f19ef185d9437433a94353b9
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-19 17:59:13 -0400

    feat: config import eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,52 +8,52 @@ export default [
   {languageOptions: { globals: {...globals.browser, ...globals.node} }, 
   ignorePatterns: ['eslint.config.mjs'],
   rules: {
-    // '@typescript-eslint/no-unnecessary-condition': [
-    //   'error',
-    //   { allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing: true },
-    // ],
-    // '@typescript-eslint/interface-name-prefix': 'off',
-    // '@typescript-eslint/explicit-function-return-type': 'off',
-    // '@typescript-eslint/explicit-module-boundary-types': 'off',
-    // '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unnecessary-condition': [
+      'error',
+      { allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing: true },
+    ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
 
-    // 'import/no-unresolved': 'error',
-    // 'sort-imports': [
-    //   'error',
-    //   {
-    //     ignoreCase: false,
-    //     ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
-    //     ignoreMemberSort: false,
-    //     memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-    //     allowSeparatedGroups: true,
-    //   },
-    // ],
-    // 'import/order': [
-    //   'error',
-    //   {
-    //     groups: [
-    //       'builtin',
-    //       'external',
-    //       'internal',
-    //       ['sibling', 'parent'],
-    //       'index',
-    //       'unknown',
-    //     ],
-    //     'newlines-between': 'always',
-    //     alphabetize: {
-    //       order: 'asc',
-    //       caseInsensitive: true,
-    //     },
-    //   },
-    // ],
+    'import/no-unresolved': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
-  // settings: {
-  //   'import/resolver': {
-  //     typescript: {
-  //       project: './tsconfig.json',
-  //     },
-  //   },
-  // },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
 },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
