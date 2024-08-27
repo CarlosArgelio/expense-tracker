1b387453ec4a9e4ba8e665f9fb91acef963fd8d3
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-27 18:57:50 -0400

    feat: import/exports added

---
+++ src/components/expense-tracker/domain/index.ts
@@ -0,0 +1,6 @@
+// interfaces
+export { IExpense, ICreateExpense, IExpenseTrackerDAO, IExpenseTrackerRepository, IUpdateExpense } from "./interfaces";
+// entity
+export { Expense } from "./entity";
+// services
+export { ExpenseTrackerService } from "./service";
